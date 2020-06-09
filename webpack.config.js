e787c6f37d738d43fac22d6ef38079e407202365
Tommoore96 <tommoore.dev@gmail.com>
2020-06-09 15:54:32 +0100

    working as a single import map

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+const path = require("path");
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "i6",
+    projectName: "single-spa-cra",
+    webpackConfigEnv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    devServer: {
+      disableHostCheck: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+        // {
+        //   test: /\.css$/i,
+        //   use: ["style-loader", "css-loader"],
+        //   // exclude: [path.resolve("src")],
+        // },
+      ],
+    },
+  });
+};
