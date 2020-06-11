e44abb67795d66b690c2402861b3a31266262348
Tommoore96 <tommoore.dev@gmail.com>
2020-06-11 13:23:59 +0100

    styles added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="single-spa-cra">
+      <h2>Single SPA test with CRA</h2>
+      This is active across all routes. Imagine it as the Header and MenuBar of
+      Business Aviation
+      <br />
+      <a href="http://localhost:3000/business-aviation">
+        Activate "business-aviation"
+      </a>
+      <hr />
+    </div>
+  );
 }
 
 export default App;
